b54b4f278d1d8b94adbda72a71c9017d52bf73d2
nireo <eemil.lehtikangas1@gmail.com>
2024-07-24 12:15:31 +0300

    Added HTTP request parsing

--- stupidhttp.go
+++ stupidhttp.go
@@ -1,10 +1,13 @@
 package stupidhttp
 
 import (
-	"bytes"
+	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 )
 
 type MethodType int
@@ -16,11 +19,16 @@ const (
 	MethodPost
 	MethodPut
 	MethodDelete
+	MethodUnrecognized
 )
 
 type Request struct {
-	Headers map[string]string
-	Body    io.ReadCloser
+	Headers    map[string]string
+	Body       io.ReadCloser
+	Method     MethodType
+	ProtoMajor int
+	ProtoMinor int
+	Path       string
 }
 
 type Response struct {
@@ -34,9 +42,14 @@ type Route struct {
 	HandleFunc func(*Request) (*Response, error)
 }
 
+type Config struct {
+	MaxHeaderSize int
+}
+
 type Server struct {
 	Routes  map[string]Route
 	Address string
+	Config  Config
 }
 
 func (s *Server) AddHandler(path string, method MethodType, handlerFunc HandleFunc) {
@@ -64,17 +77,127 @@ func (s *Server) Start() error {
 	}
 }
 
-func parseRequest(c net.Conn) (*Request, error) {
-	// TODO: make this more efficient since this is going too nicely with the name otherwise
+func strToMethodType(method string) MethodType {
+	switch method {
+	case "GET":
+		return MethodGet
+	case "POST":
+		return MethodGet
+	case "DELETE":
+		return MethodGet
+	case "PUT":
+		return MethodGet
+	default:
+		return MethodUnrecognized
+	}
+}
 
-	// Basically the header and content are separated by \r\n\r\n and if the request is valid
-	// it should be the first instance.
-	sepIndex := bytes.Index()
+func (s *Server) parseRequest(c net.Conn) (*Request, error) {
+	// TODO: make this more efficient since this is going too nicely with the name otherwise
+	reader := bufio.NewReader(c)
+
+	reqLine, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, fmt.Errorf("error reading request line: %w", err)
+	}
+	reqLine = strings.TrimSpace(reqLine)
+	splitted := strings.Split(reqLine, " ")
+	if len(splitted) != 3 {
+		return nil, fmt.Errorf("invalid request line")
+	}
+
+	major, minor, err := parseHTTPProto(splitted[2])
+	if err != nil {
+		return nil, fmt.Errorf("invalid proto: %w", err)
+	}
+
+	method := strToMethodType(splitted[0])
+	if method == MethodUnrecognized {
+		return nil, fmt.Errorf("invalid method: %w", err)
+	}
+
+	headers := make(map[string]string)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, fmt.Errorf("error reading header: %w", err)
+		}
+
+		line = strings.TrimSpace(line)
+		// the headers end and content starts
+		if line == "" {
+			break
+		}
+
+		splitted := strings.SplitN(line, ":", 2)
+		if len(splitted) != 2 {
+			return nil, fmt.Errorf("invalid header: %s", line)
+		}
+
+		headers[strings.TrimSpace(splitted[0])] = strings.TrimSpace(splitted[1])
+	}
+
+	var body io.ReadCloser
+	if clen, ok := headers["Content-Length"]; ok {
+		l, err := strconv.Atoi(clen)
+		if err != nil {
+			return nil, fmt.Errorf("invalid content-length: %w", err)
+		}
+
+		body = io.NopCloser(io.LimitReader(reader, int64(l)))
+	} else {
+		body = io.NopCloser(reader)
+	}
+
+	return &Request{
+		Body:       body,
+		Headers:    headers,
+		Method:     method,
+		ProtoMajor: major,
+		ProtoMinor: minor,
+	}, nil
+}
+
+// parseHTTPProto also validates that the protocol is valid
+func parseHTTPProto(proto string) (int, int, error) {
+	if !strings.HasPrefix(proto, "HTTP/") {
+		return 0, 0, fmt.Errorf("unsupported protocol")
+	}
+
+	version := strings.TrimPrefix(proto, "HTTP/")
+	parts := strings.Split(version, ".")
+	if len(parts) != 2 {
+		return 0, 0, fmt.Errorf("invalid http version format")
+	}
+
+	major, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0, 0, fmt.Errorf("invalid major version: %w", err)
+	}
+
+	minor, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("invalid minor version: %w", err)
+	}
+	return major, minor, nil
+}
+
+func (s *Server) returnResponse(resp *Response) error {
+	return nil
 }
 
 func (s *Server) handleConn(c net.Conn) error {
 	// TODO: Construct Request object aka parse the request
 	// TODO: Decide which route should handle given request
 	// TODO: Execute the handler func for the given request and write the response
+
+	defer c.Close()
+
+	request, err := s.parseRequest(c)
+	if err != nil {
+		// TODO: return p
+		return err
+	}
+
 	return nil
 }
